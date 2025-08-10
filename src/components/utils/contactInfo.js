a79d65185a8043e8541eb992978eb3a1392a04e3
azim128 <azimruet28@gmail.com>
2025-08-11 00:33:14 +0600

    feat(setup): update info

---
+++ src/components/utils/contactInfo.js
@@ -0,0 +1,10 @@
+const contactInfo = {
+  email: "ollepunauki@outlook.com",
+  phone: "+1 208-278-7545",
+  address: "5205 NE 2ND AVE, Miami, FL 33137, United States",
+  businessHours: "Mon-Fri: 9:00 AM - 6:00 PM EST",
+  contactPerson: "Alexander Kenneth Meyer",
+  contactPersonRole: "Executive Leadership",
+};
+
+export default contactInfo;
