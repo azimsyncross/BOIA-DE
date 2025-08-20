b772b1ed3b8b577c2b65084029b025ffabad1b52
azimsyncross <azimsyncross@gmail.com>
2025-08-20 09:48:45 +0600

    change contact map

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -345,7 +345,7 @@ const ContactPage = () => {
           </p>
           <div className="rounded-xl overflow-hidden shadow-2xl shadow-purple-500/10 border border-gray-700">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3591.377417113341!2d-80.19391772582219!3d25.82410600602593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b15ef5d557b7%3A0x4d2a494faa359834!2s5205%20NE%202nd%20Ave%2C%20Miami%2C%20FL%2033137%2C%20USA!5e0!3m2!1sen!2sbd!4v1754844343621!5m2!1sen!2sbd"
+              src={contactInfo.location}
               width="100%"
               height="450"
               style={{ border: 0 }}
--- src/components/utils/contactInfo.js
+++ src/components/utils/contactInfo.js
@@ -5,6 +5,9 @@ const contactInfo = {
   businessHours: "Mon-Fri: 9:00 AM - 6:00 PM EST",
   contactPerson: "Ramiro Fregoso",
   contactPersonRole: "Executive Leadership",
+  location:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2814.4915598485873!2d-122.85712712354524!3d45.136637971070506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54955c20ee7747cd%3A0x5635a9d649422374!2s449%20Marshall%20St%2C%20Woodburn%2C%20OR%2097071%2C%20USA!5e0!3m2!1sen!2sbd!4v1755657132227!5m2!1sen!2sbd"
 };
+
+
 
 export default contactInfo;
